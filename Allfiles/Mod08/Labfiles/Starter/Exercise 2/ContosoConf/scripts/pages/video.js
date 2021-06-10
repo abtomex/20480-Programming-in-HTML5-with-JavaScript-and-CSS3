e21e302623f866c218e61b276370199d575bab51
dchechumaev <dmitriy.chechumaev@v6services.ru>
2021-06-10 14:09:39 +0300

    Exercise 2

--- Allfiles/Mod08/Labfiles/Starter/Exercise 2/ContosoConf/scripts/pages/video.js
+++ Allfiles/Mod08/Labfiles/Starter/Exercise 2/ContosoConf/scripts/pages/video.js
@@ -12,22 +12,30 @@ const time = videoSection.querySelector(".video-time");
 
 function ready() {
     // TODO: display the video controls
+    controls.style.display = "block";
 
 };
 
 function play() {
     // TODO: play the video
+    video.play();
+    playButton.style.display = "none";
+    pauseButton.style.display = "";
 
 };
 
 function pause() {
     // TODO: pause the video
+    video.pause();
+    pauseButton.style.display = "none";
+    playButton.style.display = "";
 
 };
 
 function updateTime() {
     // TODO: Set time.textContent using video.current time.
     //       Use the formatTime function to convert raw seconds into HH:MM:SS format.
+    time.textContent = formatTime(video.currentTime);
 
 };
 
@@ -35,9 +43,13 @@ pauseButton.style.display = "none";
 
     // TODO: Add event listeners for:
     //       video loaddata
+video.addEventListener("loadeddata", ready, false);
     //       video timeupdate
+video.addEventListener("timeupdate", updateTime, false);
     //       play click
+playButton.addEventListener("click", play, false);
     //       pause click
+pauseButton.addEventListener("click", pause, false);
 
 // SIG // Begin signature block
 // SIG // MIIaVgYJKoZIhvcNAQcCoIIaRzCCGkMCAQExCzAJBgUr
